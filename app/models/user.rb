e704cf1c4781d27640ce38de70f443d3fe359f56
Alexandre Rinaldi <alexandre.rinaldi@essec.edu>
2020-02-19 11:19:25 +0100

    add-photo-to-navbar-and-sign-up

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
+  has_one_attached :photo
   has_many :places, dependent: :destroy
   has_many :user_reviews
   has_many :place_reviews
